29d234b834e3472562d5646a0c059a47bf99f422
Lucas Negreiros <lnpandre0503@gmail.com>
2026-09-11 11:07:03 -0300

    refactor(frontend): garante atualizacao da lista de ultimos pedidos ao concluir uma mesa

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -10,55 +10,53 @@ import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 interface Props {
-    orders: OrderProps[]
+  orders: OrderProps[]
 }
 
 export function Orders({ orders }: Props) {
-    const { isOpen, onRequestOpen } = use(OrderContext)
-    const router = useRouter()
+  const { isOpen, onRequestOpen } = use(OrderContext)
+  const router = useRouter()
 
-    async function handleDetailOrder(order_id: string) {
-        await onRequestOpen(order_id)
-    }
+  async function handleDetailOrder(order_id: string) {
+    await onRequestOpen(order_id)
+  }
 
-    function handleRefresh() {
-        router.refresh()
-        toast.success("Pedidos atualizados com sucesso!")
-    }
+  function handleRefresh() {
+    router.refresh()
+    toast.success("Pedidos atualizados com sucesso!")
+  }
 
-    return(
-        <>
-        <main className={styles.container}>
+  return (
+    <>
+      <main className={styles.container}>
+        <section className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button onClick={handleRefresh}>
+            <RefreshCw size={24} color='#3fffa3' />
+          </button>
+        </section>
 
-            <section className={styles.containerHeader}>
-                <h1>Últimos pedidos</h1>
-                <button onClick={handleRefresh}>
-                    <RefreshCw size={24} color='#3fffa3'/>
-                </button>
-            </section>
+        <section className={styles.listOrders}>
+          {orders.length === 0 && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
 
-            <section className={styles.listOrders}>
+          {orders.map(order => (
+            <button
+              key={order.id}
+              className={styles.orderItem}
+              onClick={() => handleDetailOrder(order.id)}
+            >
+              <div className={styles.tag}></div>
+              <span>Mesa {order.table}</span>
+            </button>
+          ))}
+        </section>
+      </main>
 
-                {orders.length === 0 && (
-                    <span className={styles.emptyItem}>
-                        Nenhum pedido aberto no momento...
-                    </span>
-                )}
-
-                {orders.map( order => (
-                    <button
-                    key={order.id}
-                    className={styles.orderItem}
-                    onClick={() => handleDetailOrder(order.id)}
-                    >
-                        <div className={styles.tag}></div>
-                        <span>Mesa {order.table}</span>
-                    </button>
-                ) )}
-            </section>
-        </main>
-
-        { isOpen && <Modalorder/> }
-        </>
-    )
+      {isOpen && <Modalorder />}
+    </>
+  )
 }
